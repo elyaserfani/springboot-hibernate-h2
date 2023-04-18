a6658d613f376e970f76335d4ea6707cc148f832
Dotin <elyaserfani2@gmail.com>
2023-04-18 12:48:00 +0430

    Some minor changes

--- src/main/java/com/simplespringboot/app/security/AuthTokenFilter.java
+++ src/main/java/com/simplespringboot/app/security/AuthTokenFilter.java
@@ -37,8 +37,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
             if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
                 String username = jwtUtils.getUserNameFromJwtToken(jwt);
                 UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities());
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
